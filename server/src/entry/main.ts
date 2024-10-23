53ece36d9ca9e7ef757035815a89c1f619da29a3
razor <acm09@yandex.ru>
2024-10-23 22:11:31 +0300

    update

--- server/src/entry/main.ts
+++ server/src/entry/main.ts
@@ -1,8 +1,11 @@
 import Fastify from 'fastify'
 import { host, port, prefix } from '../../../manager/config/server.json'
 import { router } from './router'
+import { scheduler } from './scheduler'
 
 const fastify = Fastify()
 
 fastify.register(router, { prefix })
+fastify.register(scheduler)
+
 fastify.listen({ host, port })
---
+++ server/src/entry/scheduler.ts
@@ -0,0 +1,3 @@
+import { FastifyInstance } from 'fastify'
+
+export const scheduler = async (_: FastifyInstance): Promise<void> => {}
